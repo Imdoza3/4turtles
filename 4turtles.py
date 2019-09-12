a5af65fd7fad1e7c23d770aa5ef4acfcf6f175cf
Imdoza3 <iamsteel2004@gmail.com>
2019-09-12 08:36:58 -0500

    Initial commit

---
+++ 4turtles.py
@@ -0,0 +1,22 @@
+import turtle
+
+isaiah = turtle.Turtle()
+paige = turtle.Turtle()
+dyson = turtle.Turtle()
+jarrett = turtle.Turtle()
+isaiah.forward(100)
+jarrett.right(90)
+jarrett.forward(100)
+paige.right(180)
+paige.forward(100)
+dyson.left(90)
+dyson.forward(100)
+turtle.left(90)
+def quartercircle():
+     for i in range (1,5):
+         turtle.circle(100)
+         turtle.left(90)
+quartercircle()
+isaiah.()
+
+turtle.exitonclick()
